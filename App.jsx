35004e10e002ea93d73cff56d3186c6a27d56beb
Avesta <92775375+Avestair@users.noreply.github.com>
2023-11-25 20:58:42 +0330

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './assets/Navbar';
+import Main from './assets/Main';
+
+function App() {
+
+
+  return (
+    <>
+
+   <Main />
+    </>
+  )
+}
+
+export default App
